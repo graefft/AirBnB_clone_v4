5e8352e813f335d3c56428d12ff7394ccddea815
Thomas Graeff <676@holbertonschool.com>
2019-09-30 08:00:55 +0000

    Fix: got red dot to show for API status success

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,22 +1,25 @@
-$(function(){
+$(function () {
     const id_store = {}
     $('input:checkbox').change(function(){
         if($(this).is(":checked")){
-            id_store[$(this).data-id] = $(this).data-name;
+            id_store[$(this).data('id')] = $(this).data('name');
         }
         else if($(this).is(":not(:checked)")){
-            delete id_store[$(this).data-id];
+            delete id_store[$(this).data('id')];
         }
-        let checkamen = Object.values(id_store);
+        let checkamen = Object.values(id_store).join(', ');
         $('div.amenities h4').text(checkamen);
     });
 });
 
-    $.get('http://0.0.0.0:5001/api/v1/status/' function (data, status) {
-        if (status === 'OK') {
+$(function () {
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
+        if (status === 'success') {
+          if (data.status === 'OK') {
             $('#api_status').addClass('available');
-        } else {
+          } else {
             $('#api_status').removeClass('available');
+          }
         }
     });
 });
