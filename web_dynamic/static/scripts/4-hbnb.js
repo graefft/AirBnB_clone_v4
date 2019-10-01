8cefcb75c0cc9be022e79bea2447fe561bbefab2
Thomas Graeff <676@holbertonschool.com>
2019-10-01 06:49:31 +0000

    Adds if/else to keep space in h4 when unchecking

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,40 +1,39 @@
 $(function () {
-    let id_store = {}
-    $('input:checkbox').change(function(){
-        if($(this).is(":checked")){
-            id_store[$(this).data('id')] = $(this).data('name');
-        }
-        else if($(this).is(":not(:checked)")){
-            delete id_store[$(this).data('id')];
-        }
-        let checkamen = Object.values(id_store).join(', ');
-        $('div.amenities h4').text(checkamen);
-    });
+  const id_store = {};
+  $('input:checkbox').change(function () {
+    if ($(this).is(':checked')) {
+      id_store[$(this).data('id')] = $(this).data('name');
+    } else if ($(this).is(':not(:checked)')) {
+      delete id_store[$(this).data('id')];
+    }
+    if (Object.keys(id_store).length === 0) {
+      $('div.amenities h4').html('&nbsp');
+    } else {
+      const checkamen = (Object.values(id_store).join(', '));
+      $('div.amenities h4').text(checkamen);
+    }
+  });
 });
-
 
 $(function () {
   $.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
-        if (status === 'success') {
-          if (data.status === 'OK') {
-            $('#api_status').addClass('available');
-          } else {
-            $('#api_status').removeClass('available');
-          }
-        }
-    });
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
 });
 
-
-$(function fetchPlaces(ids) {
-    $.ajax({
-        type: 'POST',
-        url: 'http://0.0.0.0:5001/api/v1/places_search/',
-        contentType: 'application/json',
-        data: JSON.stringify(ids),
-        success: function(result) {
-            for (let place of result){
-                let articleTag = '<article>' + '<div class="title">' +
+$(function fetchPlaces (ids) {
+  $.ajax({
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    contentType: 'application/json',
+    data: JSON.stringify(ids),
+    success: function (result) {
+      for (const place of result) {
+        const articleTag = '<article>' + '<div class="title">' +
                 '<h2>' + place.name + '</h2>' + '<div class="price_by_night">' +
                 '$' + place.price_by_night + '</div>' + '</div>' +
                 '<div class="information">' + '<div class="max_guest">' +
@@ -49,17 +48,17 @@ $(function fetchPlaces(ids) {
                 '</div>' + '</div>' + '<div class="user">' +
 
                 '<div class="description">' + place.description + '</div>' + '</article>';
-                $('section.places').append(articleTag);
-           }
-        }
-    });
+        $('section.places').append(articleTag);
+      }
+    }
+  });
 });
 
 fetchAmens({});
 
-$('p').click(function() {
-    let myDict = {};
-    myDict['amenities'] = Object.keys(id_store);
-    fetchAmens(myDict);
-    console.log('Success');
+$('p').click(function () {
+  const myDict = {};
+  myDict.amenities = Object.keys(id_store);
+  fetchAmens(myDict);
+  console.log('Success');
 });
